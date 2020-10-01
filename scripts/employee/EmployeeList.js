e144b0cd31126bdcb297d7ce43d2d349df2f94e8
Cornelia Johnson <corneliaj91@gmail.com>
2020-10-01 12:36:10 -0400

    fetched computers

--- scripts/employee/EmployeeList.js
+++ scripts/employee/EmployeeList.js
@@ -1,5 +1,12 @@
 import { getEmployees, useEmployees } from "./EmployeeProvider.js";
 
 export const EmployeeList = () => {
-  getEmployees().then(() => useEmployees());
+  getEmployees().then(() => {
+    let employeeArray = useEmployees();
+    render(employeeArray);
+  });
+};
+
+const render = (theEmployeeArray) => {
+  console.log(theEmployeeArray);
 };
--- scripts/employee/EmployeeProvider.js
+++ scripts/employee/EmployeeProvider.js
@@ -1,11 +1,11 @@
-let empoyees = [];
+let employees = [];
 
-export const useEmployees = () => empoyees.slice();
+export const useEmployees = () => employees.slice();
 
 export const getEmployees = () => {
   return fetch("http://localhost:8088/employees")
     .then((response) => response.json())
-    .then((parsedEmpoyees) => {
-      empoyees = parsedEmpoyees;
+    .then((parsedEmployees) => {
+      employees = parsedEmployees;
     });
 };
